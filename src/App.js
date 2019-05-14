30725af510f2453dd5df784eb1ef2eed30cf935e
Mikko Översti <moversti92@gmail.com>
2019-05-14 11:53:37 +0300

    add headers

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
   return (
     <div className="App">
       <div className='areacontainer'>
+      <h5>Romaji:</h5>
         <Boksi to="r" value={value} setValue={setValue} />
       </div>
       <div className='areacontainer'>
+      <h5>Hiragana:</h5>
         <Boksi to="k" value={value} setValue={setValue} />
       </div>
     </div>
